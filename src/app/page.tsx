ffe50820eda2604963d09050b4643d2a5e09d528
kty <xodus776@nate.com>
2023-08-08 12:57:05 +0900

    수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,32 +7,30 @@ export default function Home() {
   const { data: session } = useSession();
 
   return (
-    <main>
-      <div className="flex gap-2 ml-auto">
-        <div>
-          <Link
-            href="/about"
-          >
-            About
-          </Link>
-        </div>
-        {session?.user ? (
-          <>
-            <img
-              className="w-8 h-8 rounded-full"
-              src={session.user.image || ""}
-            />{session.user.name || ""}
-            <p className="text-sky-600"> {session.user.email}</p>
-            <button className="text-red-500" onClick={() => signOut()}>
-              Sign Out
-            </button>
-          </>
-        ) : (
-          <button className="text-green-600" onClick={() => signIn()}>
-            Sign In
-          </button>
-        )}
+    <div className="flex gap-2 ml-auto">
+      <div>
+        <Link
+          href="/about"
+        >
+          About
+        </Link>
       </div>
-    </main>
+      {session?.user ? (
+        <>
+          <img
+            className="w-8 h-8 rounded-full"
+            src={session.user.image || ""}
+          />{session.user.name || ""}
+          <p className="text-sky-600"> {session.user.email}</p>
+          <button className="text-red-500" onClick={() => signOut()}>
+            Sign Out
+          </button>
+        </>
+      ) : (
+        <button className="text-green-600" onClick={() => signIn()}>
+          Sign In
+        </button>
+      )}
+    </div>
   );
 }
