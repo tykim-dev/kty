492ecd8b88b81c58f3489002273b061828053bf3
kty <xodus776@nate.com>
2023-08-22 22:38:19 +0900

    수정

--- src/app/api/htmlparser/route.ts
+++ src/app/api/htmlparser/route.ts
@@ -1,57 +1,57 @@
 'use strict'
 import User from "@/app/models/userModel";
+import { Word1, Word2, Word3, Word4, Word5 } from "@/app/models/word1Model";
 import Word from "@/app/models/wordModel";
 import connectDB from "@/app/utils/database";
 import { NextRequest, NextResponse } from "next/server"
 
 
 // 1: 3246, 2: 2648, 3: 1546, 4: 1037, 5: 744
-const DATA_USERS_URL = `https://ja.dict.naver.com/api/jako/getJLPTList?level=2&part=%EC%A0%84%EC%B2%B4&page=1`
-
-type WordInfo = {
-  category1: String,
-  level: String, 
-  pron: String, 
-  entry: String,
-  means: Array<String>,
-  parts: Array<String>,
-}
+const DATA_USERS_URL = `https://ja.dict.naver.com/api/jako/getJLPTList?level=4&part=%EC%A0%84%EC%B2%B4&page=1`
 
 export async function GET(request: NextRequest) {
 
-  const res = await fetch(DATA_USERS_URL);
-  const data = await res.json();
+  // const res = await fetch(DATA_USERS_URL);
+  // const data = await res.json();
 
-  // const { m_total, m_page, m_pageSize, m_start, m_end, m_totalPage, m_items } = data;
-  const { m_total, m_totalPage } = data;
+  // // const { m_total, m_page, m_pageSize, m_start, m_end, m_totalPage, m_items } = data;
+  // const { m_total, m_totalPage } = data;
 
-  await connectDB();
+  // await connectDB();
   
-  for (let index = 0; index < m_totalPage; index++) {
-    let url = `https://ja.dict.naver.com/api/jako/getJLPTList?level=2&part=%EC%A0%84%EC%B2%B4&page=${index + 1}`;
+  // for (let index = 0; index < m_totalPage; index++) {
+  //   let url = `https://ja.dict.naver.com/api/jako/getJLPTList?level=4&part=%EC%A0%84%EC%B2%B4&page=${index + 1}`;
     
-    let resData = await fetch(url);
-    let resJson = await resData.json();
-    const { m_items } = resJson;
+  //   let resData = await fetch(url);
+  //   let resJson = await resData.json();
+  //   const { m_items } = resJson;
 
-    for (let idx = 0; idx < m_items.length; idx++) {
-    // await m_items.forEach( async (wordInfo: WordInfo) => {
-      const { category1, level, pron, show_entry, means, parts } = m_items[idx];
+  //   for (let idx = 0; idx < m_items.length; idx++) {
+  //   // await m_items.forEach( async (wordInfo: WordInfo) => {
+  //     const { category1, level, pron, show_entry, means, parts } = m_items[idx];
 
-      const newWord = new Word({
-        type: category1 || '',
-        level: level || '',
-        word: pron || '',
-        read: show_entry || '',
-        means: means || [],
-        parts: parts || [],
-      })
+  //     const newWord = new Word4({
+  //       type: category1 || '',
+  //       level: level || '',
+  //       word: pron || '',
+  //       read: show_entry || '',
+  //       means: means || [],
+  //       parts: parts || [],
+  //     })
 
-      await newWord.save();
+  //     await newWord.save();
 
-    // });
-    }
+  //   // });
+  //   }
+  // }
+
+  // return NextResponse.json({m_total})
+
+  const word = await Word1.find();
+
+  for (let index = 0; index < word.length; index++) {
+    await new Word(word[index]).save();
   }
 
-  return NextResponse.json({m_total})
+  return NextResponse.json({word1: word.length})
 }
---
+++ src/app/models/word1Model.ts
@@ -0,0 +1,46 @@
+import { Schema, model, models } from 'mongoose'
+
+const word1Schema = new Schema({
+  // 단어구분
+  type: {
+    type: String,
+    required: true,
+    index: true,
+  },
+  level: {
+    type: String,
+    required: true,
+    index: true,
+  },
+  word: {
+    type: String,
+    required: false,
+    index: true,
+  },
+  read: {
+    type: String,
+    required: true,
+  },
+  means: {
+    type: Array,
+    required: true,
+  },
+  parts: {
+    type: Array,
+    required: false,
+  },
+}, {timestamps: true})
+
+const Word1 = models.word1 || model('word1', word1Schema)
+const Word2 = models.word2 || model('word2', word1Schema)
+const Word3 = models.word3 || model('word3', word1Schema)
+const Word4 = models.word4 || model('word4', word1Schema)
+const Word5 = models.word5 || model('word5', word1Schema)
+
+export {
+  Word1,
+  Word2,
+  Word3,
+  Word4,
+  Word5,
+};
