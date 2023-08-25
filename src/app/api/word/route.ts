b23c4627afdd33080928ea7faf5f69727002c2f3
kty <xodus776@nate.com>
2023-08-25 12:30:55 +0900

    수정

--- src/app/api/word/route.ts
+++ src/app/api/word/route.ts
@@ -4,15 +4,22 @@ import connectDB from "@/app/utils/database";
 import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(request: NextRequest) {
-  // await connectDB();
-console.log(request);
-//   const { searchParams } = new URL(request.url);
-// console.log(searchParams);
-  // const wordList = Word.find();
+  await connectDB();
+
+  const { searchParams } = new URL(request.url);
+  const type = searchParams.get('type');
+  const level = searchParams.get('level');
+  const limit = Number(searchParams.get('limit')) || 20;
+  const page = Number(searchParams.get('page') || 1);
+
+  const wordList = await Word.find({ type, level })
+    .limit(limit * 1)
+    .skip((page - 1) * limit)
+    .exec();
 
   // const userList = await User.find().select('-password');
 
-  return NextResponse.json({})
+  return NextResponse.json(wordList)
 }
 
 export async function POST(request: NextRequest) {
