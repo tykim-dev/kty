cd4e2635b9b74a977ddc8920727113786107bc87
kty <xodus776@nate.com>
2023-08-29 12:46:29 +0900

    수정

---
+++ src/app/widgets/layout/sidenav.tsx
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+import { Link, NavLink } from "react-router-dom";
+import { Heroicon } from 'heroicons'
+import {
+  Avatar,
+  Button,
+  IconButton,
+  Typography,
+} from "@material-tailwind/react";
+
+const Sidenav = () => {
+
+  return (
+    <div className="flex gap-2 ml-auto">
+      DashboardPage
+    </div>
+  )
+}
+
+export default Sidenav
