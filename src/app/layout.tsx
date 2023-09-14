e75bc85fdb9ae7c558c242d6a386db8a226357d9
kty <xodus776@nate.com>
2023-09-14 13:10:20 +0900

    수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import SessionProvider from "@/app/providers/SessionProvider";
 import '@/app/globals.css'
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import '@/app/style/tailwind.css'
-import { SWRProvider } from "./providers/SwrProvider";
+import { SWRProvider } from "./providers/SWRProvider";
 // import { Nanum_Gothic } from "next/font/google";
 
 // const nanum_gothic = Nanum_Gothic({
