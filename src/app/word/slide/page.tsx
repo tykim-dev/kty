f56bbbdbbafb1e4d1ac18e3ba76796950edf65a4
kty <xodus776@nate.com>
2023-11-30 12:52:54 +0900

    수정

--- src/app/word/slide/page.tsx
+++ src/app/word/slide/page.tsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from 'react'
 import { signIn, signOut, useSession } from "next-auth/react";
 import WordLayout from '@/app/components/Layout/WordLayout'
-import { Button, Card, CardBody, Carousel, Dialog, DialogBody, DialogHeader, Typography } from "@material-tailwind/react";
+import { Button, Card, CardBody, Carousel, Dialog, DialogBody, DialogFooter, DialogHeader, Typography } from "@material-tailwind/react";
 import useWord from '@/app/swr/useWord';
 import WordContent from './components/WordContent';
 import SearchBar from './components/SearchBar';
@@ -31,14 +31,16 @@ const SlidePage = () => {
         <WordContent conditions={conditions} />
       </div>
 
+      
       <Button onClick={handleOpen} variant="gradient">
         전체화면
       </Button>
 
       <Dialog
-        className='h-full w-full p-0 m-0'
+        className='h-full w-full p-0 m-0 bg-white'
         open={open}
         handler={handleOpen}
+        
         animate={{
           mount: { scale: 1, y: 0 },
           unmount: { scale: 0.9, y: -100 },
@@ -46,8 +48,24 @@ const SlidePage = () => {
       >
         <DialogHeader>Its a simple dialog.</DialogHeader>
         <DialogBody>
-          aaaaaaaaaaaaaaaaaaaaaaaaaa
+          The key to more success is to have a lot of pillows. Put it this way,
+          it took me twenty five years to get these plants, twenty five years of
+          blood sweat and tears, and I&apos;m never giving up, I&apos;m just
+          getting started. I&apos;m up to something. Fan luv.
         </DialogBody>
+        <DialogFooter>
+          <Button
+            variant="text"
+            color="red"
+            onClick={handleOpen}
+            className="mr-1"
+          >
+            <span>Cancel</span>
+          </Button>
+          <Button variant="gradient" color="green" onClick={handleOpen}>
+            <span>Confirm</span>
+          </Button>
+        </DialogFooter>
       </Dialog>
     </WordLayout>
   )
