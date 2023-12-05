216dc944f965865e10c3de0c092f51ee1cfa5650
kty <xodus776@nate.com>
2023-12-05 12:48:07 +0900

    수정

--- src/app/word/slide/components/WordCard.tsx
+++ src/app/word/slide/components/WordCard.tsx
@@ -1,6 +1,5 @@
 'use client'
 import React, {memo} from 'react';
-import { Typography } from '@material-tailwind/react';
 import { SwiperSlide } from 'swiper/react';
 
 type WordCardProps = {
@@ -15,19 +14,17 @@ const WordCard = (props: WordCardProps) => {
 
   return (
     <>
-      {/* <SwiperSlide> */}
-        <div className="h-96 text-center flex flex-col items-center justify-center">
-            <Typography variant="h4" color="blue-gray" className="mb-2">
-              {wordInfo?.word}
-            </Typography>
-          <Typography variant="h4" color="blue-gray" className="mb-2">
-            {wordInfo?.read}
-          </Typography>
-          <Typography variant="h4" color="blue-gray" className=''>
-            {wordInfo?.means}
-          </Typography>
-        </div>
-      {/* </SwiperSlide> */}
+      <div className="h-screen text-center flex flex-col items-center justify-center">
+        <h3 className="text-4xl font-normal leading-normal mt-0 mb-2 text-blueGray-800">
+          {wordInfo?.word}
+        </h3>
+        <h3 className="text-4xl font-normal leading-normal mt-0 mb-2 text-blueGray-800">
+          {wordInfo?.read}
+        </h3>
+        <h3 className="text-4xl font-normal leading-normal mt-0 mb-2 text-blueGray-800">
+          {wordInfo?.means}
+        </h3>
+      </div>
     </>
   )
 }
--- src/app/word/slide/components/WordContent.tsx
+++ src/app/word/slide/components/WordContent.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { useState } from 'react';
 import useWord from '@/app/swr/useWord';
 import { Button, Card, CardBody, Carousel, IconButton, Typography } from '@material-tailwind/react';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -23,6 +24,7 @@ const WordContent = (props: WordTableProps) => {
     conditions
   } = props
 
+  const [isFullScreen, setFullScreen] = useState<boolean>(false);
   const {data: words = [], isLoading, error} = useWord(conditions);
 
   // const {data: pageInfo} = useWordPage(conditions);
@@ -31,17 +33,33 @@ const WordContent = (props: WordTableProps) => {
   //   return <p>조회중...</p>;
   // }
 
+  const handleChangeScreen = (size: string | undefined) => {
+    setFullScreen(size === 'full');
+  }
+
   return (
     <>
-      <div className={`flex flex-wrap mt-4`}>
-        <div className="w-full mb-4 px-4">
+      <div className={`flex flex-wrap ${isFullScreen ? 'fixed z-50 w-full h-full top-0 left-0 flex items-center justify-center' : ''}`}>
+        <div className="w-full">
           <div
             className={
-              "relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded " +
-              "bg-white"
+              "relative flex flex-col min-w-0 break-words w-full shadow-lg rounded " +
+              "bg-blueGray-100 border-0"
             }
           >
-            <Swiper
+          <div className="rounded-t bg-white mb-0 px-6 py-6">
+            <div className="text-right justify-between">
+              {/* <h6 className="text-blueGray-700 text-xl font-bold">My account</h6> */}
+              <button
+                className="bg-blueGray-700 active:bg-blueGray-600 text-white font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+                type="button"
+                onClick={() => handleChangeScreen('full')}
+              >
+                전체화면
+              </button>
+            </div>
+          </div>
+          <Swiper
             // spaceBetween={30}
             centeredSlides={true}
             autoplay={{
@@ -63,7 +81,7 @@ const WordContent = (props: WordTableProps) => {
           </Swiper>
         </div>
       </div>
-      </div>
+      </div> 
     </>
   )
 }
