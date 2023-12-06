72952b85f145404281f66859b5a4d6d3740c2aa4
kty <xodus776@nate.com>
2023-12-06 12:49:14 +0900

    수정

--- src/app/word/slide/components/WordCard.tsx
+++ src/app/word/slide/components/WordCard.tsx
@@ -4,17 +4,19 @@ import { SwiperSlide } from 'swiper/react';
 
 type WordCardProps = {
   wordInfo: any,
+  fullScreen: boolean,
 }
 
 const WordCard = (props: WordCardProps) => {
 
   const {
-    wordInfo
+    wordInfo,
+    fullScreen = false,
   } = props
 
   return (
     <>
-      <div className="h-screen text-center flex flex-col items-center justify-center">
+      <div className={`${fullScreen ? 'h-[calc(100vh-80px)]' : 'h-96'} text-center flex flex-col items-center justify-center`}>
         <h3 className="text-4xl font-normal leading-normal mt-0 mb-2 text-blueGray-800">
           {wordInfo?.word}
         </h3>
--- src/app/word/slide/components/WordContent.tsx
+++ src/app/word/slide/components/WordContent.tsx
@@ -39,21 +39,16 @@ const WordContent = (props: WordTableProps) => {
 
   return (
     <>
-      <div className={`flex flex-wrap ${isFullScreen ? 'fixed z-50 w-full h-full top-0 left-0 flex items-center justify-center' : ''}`}>
-        <div className="w-full">
-          <div
-            className={
-              "relative flex flex-col min-w-0 break-words w-full shadow-lg rounded " +
-              "bg-blueGray-100 border-0"
-            }
-          >
+      <div className={`${isFullScreen ? 'fixed inset-0 z-10 overflow-y-auto h-screen' : 'flex flex-wrap mt-4'}`}>
+        <div className={`w-full ${isFullScreen ? '' : 'mb-4 px-4'}`}>
+          <div className={`relative flex flex-col min-w-0 break-words w-full shadow-lg bg-blueGray-100 border-0 ${isFullScreen ? '' : 'rounded'}`}>
           <div className="rounded-t bg-white mb-0 px-6 py-6">
-            <div className="text-right justify-between">
-              {/* <h6 className="text-blueGray-700 text-xl font-bold">My account</h6> */}
+            <div className={`text-center flex justify-between`}>
+              <h6 className="text-blueGray-700 text-xl font-bold">단어암기</h6>
               <button
                 className="bg-blueGray-700 active:bg-blueGray-600 text-white font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
                 type="button"
-                onClick={() => handleChangeScreen('full')}
+                onClick={() => handleChangeScreen(isFullScreen ? '' : 'full')}
               >
                 전체화면
               </button>
@@ -75,7 +70,7 @@ const WordContent = (props: WordTableProps) => {
           >
             {words.map((wordInfo: any, index: number) => {
               return (
-                <SwiperSlide key={index}><WordCard wordInfo={wordInfo} /></SwiperSlide>
+                <SwiperSlide key={index}><WordCard fullScreen={isFullScreen} wordInfo={wordInfo} /></SwiperSlide>
               )
             })}
           </Swiper>
