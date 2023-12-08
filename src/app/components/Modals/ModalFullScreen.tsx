e1c78d786b807850f9992c70168ce7e4a1700bba
kty <xodus776@nate.com>
2023-12-08 12:42:14 +0900

    수정

--- src/app/components/Modals/ModalFullScreen.tsx
+++ src/app/components/Modals/ModalFullScreen.tsx
@@ -22,14 +22,14 @@ const ModalFullScreen = (props: ModalFullScreenProps) => {
         <div className={`w-full ${isFullScreen ? '' : 'mb-4 px-4'}`}>
           <div className={`relative flex flex-col min-w-0 break-words w-full shadow-lg bg-blueGray-100 border-0 ${isFullScreen ? '' : 'rounded'}`}>
             <div className="rounded-t bg-white mb-0 px-6 py-6">
-              <div className={`text-center flex justify-between`}>
+              <div className={`flex flex justify-between`}>
                 <h6 className="text-blueGray-700 text-xl font-bold">{title}</h6>
                 <button
                   className="bg-blueGray-700 active:bg-blueGray-600 text-white font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
                   type="button"
                   onClick={() => handleChangeScreen(isFullScreen ? '' : 'full')}
                 >
-                  전체화면
+                  전체화면 {isFullScreen ? '취소' : ''}
                 </button>
               </div>
             </div>
