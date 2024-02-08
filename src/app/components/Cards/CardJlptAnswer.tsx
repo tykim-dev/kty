947301e57d2fb510beac241d36a4076bea32250c
kty <xodus776@nate.com>
2024-02-08 15:34:35 +0900

    수정

--- src/app/components/Cards/CardJlptAnswer.tsx
+++ src/app/components/Cards/CardJlptAnswer.tsx
@@ -10,21 +10,21 @@ type JlptAnswerProps = {
 const CardJlptAnswer = (props:JlptAnswerProps) => {
   const {questionNo, choices, answer} = props;
 
+  const parseHtml = (html: string) => {
+    return <span dangerouslySetInnerHTML={{ __html: html }} />;
+  };
+
   return (
     <>
-      <div className="relative flex flex-col min-w-0 break-words rounded mb-1">
-        <div className="flex-auto px-4 py-2">
-          <div className="flex flex-wrap">
-            {choices && sortBy(choices, 'no').map((item, idx) => {
-              return (
-                <div key={`question-answer-${questionNo}-${item?.no}`} className="flex items-center mb-4">
-                  <input id={`default-radio-${questionNo}-${item?.no}`} type="radio" value={item?.no} name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                  <label htmlFor={`default-radio-${questionNo}-${item?.no}`} className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">{`${item?.no} ${item?.content}`}</label>
-                </div>
-              )
-            })}
-          </div>
-        </div>
+      <div className="flex-auto px-4 py-2">
+        {choices && sortBy(choices, 'no').map((item, idx) => {
+          return (
+            <div key={`question-answer-${questionNo}-${item?.no}`} className="flex items-center mb-4">
+              <input id={`default-radio-${questionNo}-${item?.no}`} type="radio" value={`${item?.no}`} name={`${questionNo}`} className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+              <label htmlFor={`default-radio-${questionNo}-${item?.no}`} className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">{parseHtml(`${item?.content}`)}</label>
+            </div>
+          )
+        })}
       </div>
     </>
   );
--- src/app/components/Cards/CardJlptQuestion.tsx
+++ src/app/components/Cards/CardJlptQuestion.tsx
@@ -8,12 +8,16 @@ const CardJlptQuestion = (props:JlptQuestionProps) => {
   const {question} = props;
   const {content} = question;
 
+  const parseHtml = (html: string) => {
+    return <div dangerouslySetInnerHTML={{ __html: html }} />;
+  };
+
   return (
     <>
       <div className="relative flex flex-col min-w-0 break-words rounded mb-1">
         <div className="flex-auto px-4 py-2">
           <div className="flex flex-wrap">
-            <p>{question?.content}</p>
+            {parseHtml(question?.content)}
           </div>
         </div>
       </div>
