2527b1076070af8456d11fef696f78d5bea77247
kty <xodus776@nate.com>
2024-05-16 22:48:43 +0900

    수정

--- src/app/components/Visit/VisitHistory.tsx
+++ src/app/components/Visit/VisitHistory.tsx
@@ -4,8 +4,11 @@ import Script from "next/script";
 export default function VisitHistory() {
   return (
     <>
-      <Script id="_wau7dk">var _wau = _wau || []; _wau.push(["tab", "ht1ftl6rx4", "7dk", "right-lower"]);</Script>
-      <Script src="//waust.at/t.js"/>
+      {/* <Script id="_wau7dk">var _wau = _wau || []; _wau.push(["tab", "ht1ftl6rx4", "7dk", "right-lower"]);</Script>
+      <Script src="//waust.at/t.js"/> */}
+
+      <Script id="_waufyx">var _wau = _wau || []; _wau.push(["small", "tz0xbhbcmv", "fyx"]);</Script>
+      <Script src="//waust.at/s.js"/>
     </>
   );
 }
