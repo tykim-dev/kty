38d2d14ef4ab86bea33df66f1f060e3d8798778b
kty <xodus776@nate.com>
2024-09-13 13:05:29 +0900

    수정

--- src/app/jlpt/test/page.tsx
+++ src/app/jlpt/test/page.tsx
@@ -20,7 +20,16 @@ const JlptTestPage = () => {
         <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
           <div className="rounded-t bg-white mb-0 px-6 py-6 shadow-lg">
             <div className="text-center flex justify-between">
-              <h6 className="text-blueGray-700 text-xl font-bold">JLPT</h6>
+              <h6 className="text-blueGray-700 text-xl font-bold">
+              {
+                { 
+                  vocabulary : "文字語彙",
+                  grammar : "文法",
+                  reading : "読解",
+                  listening : "聴解",
+                }[jlptInfo.classification]
+              }
+              </h6>
               <span
                 className="bg-blueGray-700 active:bg-blueGray-600 text-white font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
               >
