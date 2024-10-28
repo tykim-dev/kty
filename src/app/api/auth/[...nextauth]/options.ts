5932fb349555425c01765360fb7c33004ecb1999
kty <xodus776@nate.com>
2024-10-28 12:42:31 +0900

    수정

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -8,7 +8,6 @@ import { compare } from 'bcryptjs'
 import User from '@/app/models/userModel'
 import connectDB from '@/app/utils/database'
 
-
 export const options: NextAuthOptions = {
   providers: [
     KakaoProvider({
